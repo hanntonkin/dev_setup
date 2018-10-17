2ed5843a6a7f089ea43b62cde8831995a0bb7f7a
hanntonkin <hann@ieee.org>
2018-10-17 22:10:21 +0100

    ros and ubuntu setup

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Development Environment Setup
 
+Collection of the setup of my work machine.
+
+
+
 ## Ubuntu
+
+Most of my work are on Ubuntu 16.04. Install here
 
 ```
 sudo apt install -y git
@@ -8,6 +14,17 @@ git clone git@github.com:hanntonkin/dev_setup.git
 . dev_setup/setup_ubuntu.sh
 ```
 
+### System tools
+* [Terminator]() with a lot of great features compared to the default terminal. 
+* [Zsh]() 
+* [OpenSSH]()
+
+### Productivity
+* [Sublime Text]() as the main text editor.
+* [Vim]() for fast editing when ssh to.
+* [Mark my works]() for markdown editing
+
+
+
 ## Git and Github setup
 * [ssh setup](https://help.github.com/articles/connecting-to-github-with-ssh/)
-
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
 #Development environment setup in ubuntu
 # TODO
-# 1) check software installed before install them
-# 2) make printingout more clean
+# 1) make printingout more clean
 #
 
 #==================================================================
 #      			      FUNCTIONS
 
-# Detect the architecture
-if [[ "$(uname -m)" = "x86_64" ]]; then
-  ARCHITECTURE="x64"
-else
-  ARCHITECTURE="x32"
-fi
-
-function print_out()
-{
-	printf "\n$@\n"
-	#exit 2
-}
-
-
 
 #==================================================================
-#      			      MAIN FUNCTION
-print_out '------------------------------------------'
-print_out ' Install git, curl, terminator '
+#      			      SYSTEM TOOLS
+echo '----- SYSTEM TOOLS -------------------------'
 
+if [ -f '/usr/bin/curl' ]; then
+	echo "Curl installed";
+else
+	sudo apt-get install -y curl 
+fi
 
-#TOOLS
-sudo apt install -y curl
-
-sudo apt-get install -y terminator
+if [ -L '/usr/bin/terminator' ]; then
+	echo "Terminator installed";
+else
+	sudo apt-get install -y terminator 
+fi
 # write force_color_prompt=yes to .bashrc
 
 
-#Text editor (VIM + Sublime)
-sudo apt-get install -y vim
+#==================================================================
+#      			      PRODUCTIVITY
+echo '----- PRODUCTIVITY -------------------------'
 
-DIR='/opt/sublime_text'
-if [ -d $DIR ]; 
+if [ -L '/usr/bin/vim' ]; then
+	echo "Vim installed";
+else
+	sudo apt-get install -y vim 
+fi
+
+if [ -d '/opt/sublime_text' ]; 
   then 
 	echo "Sublime Text 3 installed";
   else
@@ -48,11 +44,25 @@ if [ -d $DIR ];
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt-get update
 	sudo apt-get install sublime-text
-	echo "Sublime Text 3 installed";
 #install sublime https://gist.github.com/simonewebdesign/8507139
 fi
 
-sudo apt autoremove -y
+if [ -f '/usr/bin/mark-my-words' ]; then
+	echo "Mark-my-words installed";
+else
+	echo "mark-my-words not installed";
+	sudo add-apt-repository ppa:voldyman/markmywords
+	sudo apt-get update
+	sudo apt-get install -y mark-my-words
+fi
+
+
+
+
+
+
+
+# sudo apt autoremove -y
 
 
 #==================================================================
@@ -60,4 +70,18 @@ sudo apt autoremove -y
 
 #touch exists.file
 #if [ -f exists.file ] ; then echo "yes" ; else echo "no" ; fi
+
+
+# # Detect the architecture
+# if [[ "$(uname -m)" = "x86_64" ]]; then
+#   ARCHITECTURE="x64"
+# else
+#   ARCHITECTURE="x32"
+# fi
+
+# function print_out()
+# {
+# 	printf "\n$@\n"
+# 	#exit 2
+# }
 
