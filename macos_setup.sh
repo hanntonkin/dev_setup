b1efdb2d07a7330bfbaad3e04b012b66c3036d3c
Hann Nguyen <hann@ieee.org>
2018-10-18 14:48:30 +0100

    mac updated

--- macos_setup.sh
+++ macos_setup.sh
@@ -1,89 +1,43 @@
-#!/bin/bash
-#Development environment setup in ubuntu
-# TODO
+#!/usr/bin/env bash
+#Development environment setup on Mac
+# hann@ieee.org
+# REF
 # 1) make printingout more clean
 #
 
-#==================================================================
-#      			      FUNCTIONS
+
+#get admin power
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 #==================================================================
-#      			      SYSTEM TOOLS
-echo '----- SYSTEM TOOLS -------------------------'
+#      			      INSTALL APPs
+#==================================================================
 
-if [ -f '/usr/bin/curl' ]; then
-	echo "Curl installed";
-else
-	sudo apt-get install -y curl 
-fi
+#Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-if [ -L '/usr/bin/terminator' ]; then
-	echo "Terminator installed";
-else
-	sudo apt-get install -y terminator 
-fi
-# write force_color_prompt=yes to .bashrc
+brew install mas
+mas signin
+
+
+
 
 
 #==================================================================
-#      			      PRODUCTIVITY
-echo '----- PRODUCTIVITY -------------------------'
-
-if [ -L '/usr/bin/vim' ]; then
-	echo "Vim installed";
-else
-	sudo apt-get install -y vim 
-fi
-
-if [ -d '/opt/sublime_text' ]; 
-  then 
-	echo "Sublime Text 3 installed";
-  else
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt-get update
-	sudo apt-get install sublime-text
-	cp /usr/share/applications/sublime_text.desktop ~/.local/share/applications/
-#install sublime https://gist.github.com/simonewebdesign/8507139
-
-fi
-
-if [ -f '/usr/bin/mark-my-words' ]; then
-	echo "Mark-my-words installed";
-else
-	echo "mark-my-words not installed";
-	sudo add-apt-repository ppa:voldyman/markmywords
-	sudo apt-get update
-	sudo apt-get install -y mark-my-words
-fi
-
-
-
-
-
-
-
-# sudo apt autoremove -y
-
-
+#      			      SETTINGS
 #==================================================================
-#      			      REFERENCES
-
-#touch exists.file
-#if [ -f exists.file ] ; then echo "yes" ; else echo "no" ; fi
+# more here ~/osx.sh — Originally from https://mths.be/osx
 
 
-# # Detect the architecture
-# if [[ "$(uname -m)" = "x86_64" ]]; then
-#   ARCHITECTURE="x64"
-# else
-#   ARCHITECTURE="x32"
-# fi
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
 
-# function print_out()
-# {
-# 	printf "\n$@\n"
-# 	#exit 2
-# }
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+
+
+
 
